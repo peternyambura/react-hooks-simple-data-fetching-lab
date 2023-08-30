7f7af9a688a3d8fb818e6367995e222f3cdd9869
Peter Ndirangu Nyambura <138131705+peternyambura@users.noreply.github.com>
2023-08-30 20:01:34 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
-// create your App component here
+function App() {
+  // Create state variables for the dog image and loading status
+  const [dogImage, setDogImage] = useState('');
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => response.json())
+      .then((data) => {
+        setDogImage(data.message);
+        setLoading(false);
+      });
+  }, []); 
+
+  return (
+    <div className="App">
+      <h1>Random Dog Image</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
+          <img src={dogImage} alt="A Random Dog" />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
